0d2f1567dcbe873d48eb139fa2b0b644e9d83fc2
maiqlam <maiqlam@gmail.com>
2023-07-18 13:05:09 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import getAllStarships from "./services/sw-api";
+import StarShipCard from "./components/StarShipCard";
 
 function App() {
+  const [starships, setStarships] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getAllStarships();
+      console.log(data);
+      setStarships(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>STAR WARS STARSHIPS</header>
+    <div className="starshipContainer">
+    {starships.map((starship) => (
+        <StarShipCard 
+          starship={starship}
+        />
+      ))}</div>
     </div>
   );
 }
---
+++ src/components/StarShipCard.js
@@ -0,0 +1,7 @@
+export default function StarShipCard({ starship }) {
+    return (
+        <div className="shipCard">
+        <h2>{starship.name}</h2>
+        </div>
+    )
+}
